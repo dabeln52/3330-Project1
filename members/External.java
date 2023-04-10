d214838c4b3c2473d8294e0984ccc0fbd0d78f70
battleaxe5089 <108289327+battleaxe5089@users.noreply.github.com>
2023-04-10 15:25:28 -0500

    Update External.java

--- members/External.java
+++ members/External.java
@@ -1,6 +1,6 @@
 import java.sql.Date;
 
-public class External extends Person {
+public class External extends Member {
 
     public External(String name, String address, Date dob, String email, String ssn, int memberID){
         super(name, address, dob, email, ssn, memberID);
