33ea36fa0e1cd452323f8bb2641dcee1b61946f5
battleaxe5089 <108289327+battleaxe5089@users.noreply.github.com>
2023-04-12 13:33:31 -0500

    Update Member.java
    added fines

--- members/Member.java
+++ members/Member.java
@@ -2,10 +2,12 @@ import java.sql.Date;
 
 public class Member extends Person {
     private int memberID;
+    private double fines;
 
-    public Member(String name, String address, Date dob, String email, String ssn, int memberID){
+    public Member(String name, String address, Date dob, String email, String ssn, int memberID, double fines){
         super(name, address, dob, email, ssn);
         this.memberId = memberID;
+        this.fines = fines;
     }
     
     public int getMemberID(){
@@ -13,5 +15,12 @@ public class Member extends Person {
     }
     public void setMemberID(int memberID){
         this.memberID = memberID;
+    }
+    
+    public double getFines(){
+        `return fines;
+    }
+    public void setFines(double fines){
+        this.fines = fines;
     }
 }
