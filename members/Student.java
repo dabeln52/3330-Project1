103537cb52968c841b347e230035fa4c27260bd2
battleaxe5089 <108289327+battleaxe5089@users.noreply.github.com>
2023-04-12 13:38:44 -0500

    Update Student.java

--- members/Student.java
+++ members/Student.java
@@ -5,8 +5,8 @@ public class Student extends Member {
     private String Professor;
 
     // could also include GPA to determine if they get a discount, maybe add their year to but Idk how we would make that important 
-    public Student(String name, String address, Date dob, String email, String ssn, int memberID, String Professor){
-        super(name, address, dob, email, ssn, memberID);
+    public Student(String name, String address, Date dob, String email, String ssn, int memberID, double fines, String Professor){
+        super(name, address, dob, email, ssn, memberID, fines);
         this.Professor = Professor;
     }
     
